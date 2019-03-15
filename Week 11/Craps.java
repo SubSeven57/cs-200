25b777cf58c791c29de487c8098a47412eec5b56
subseven <daniel@subseven.io>
2019-03-15 16:24:22 -0500

    Made the code cuter and omtimized a method

--- Week 11/Craps.java
+++ Week 11/Craps.java
@@ -5,73 +5,64 @@ import java.util.Scanner;
 public class Craps {
 public static void main(String[] args) {
 
-  int diceOne;
-  int diceTwo;
-  int sum;
-  int point;
-  boolean play = true;
+        int diceOne;
+        int diceTwo;
+        int sum;
+        int point;
+        boolean play = true;
 
-  diceOne = getRandom();
-  diceTwo = getRandom();
+        diceOne = getRandom();
+        diceTwo = getRandom();
 
-  sum = addDice(diceOne, diceTwo);
+        sum = addDice(diceOne, diceTwo);
 
+        if ((sum == 2) || (sum == 3) || (sum == 12)) {
+                System.out.println("You lose!");
+        }
+        else if ((sum == 7) || (sum == 11)) {
+                System.out.println("You win!");
+        }
+        else {
 
-  System.out.println("You rolled a "+ diceOne + " + " + diceTwo + " = " + sum );
+                point = sum;
+                System.out.println("Point rolled is " + point);
 
-  if ((sum == 2) || (sum == 3) || (sum == 12)) {
-    System.out.println("You lose!");
-  }
+                do {
+                        diceOne = getRandom();
+                        diceTwo = getRandom();
+                        sum = addDice(diceOne, diceTwo);
 
-  else if ((sum == 7) || (sum == 11)) {
-    System.out.println("You win!");
-  }
+                        if (sum == 7) {
+                                System.out.println("You lose!");
+                                play = false;
+                        }
 
-  else {
+                        else if (sum == point) {
+                                System.out.println("You win!");
+                                play = false;
+                        }
+                } while (play == true);
+        }
 
-    point = sum;
-    System.out.println("Point rolled is " + point);
-
-    do {
-    diceOne = getRandom();
-    diceTwo = getRandom();
-    sum = addDice(diceOne, diceTwo);
-    System.out.println("You rolled a "+ diceOne + " + " + diceTwo + " = " + sum );
-
-
-    if (sum == 7) {
-      System.out.println("You lose!");
-      play = false;
-    }
-
-    else if (sum == point) {
-      System.out.println("You win!");
-      play = false;
-    }
-
-  } while (play == true);
-
-  }
-
-
-} // End of Main
+} //End of main
 
 public static int getRandom() {
-  int dice;
-  int min = 1;
-  int max = 6;
-  int range = max - min + 1;
+        int dice;
+        int min = 1;
+        int max = 6;
+        int range = max - min + 1;
 
-  dice = (int)(Math.random() * range) + min;
-  return dice;
-}
+        dice = (int)(Math.random() * range) + min;
+        return dice;
+} //End of getRandom
 
 public static int addDice(int diceOne, int diceTwo) {
-  int sum;
-  sum = diceOne + diceTwo;
-  return sum;
-}
+        int sum;
 
+        sum = diceOne + diceTwo;
 
+        System.out.println("You rolled a "+ diceOne + " + " + diceTwo + " = " + sum );
+        return sum;
+} //End of addDice
 
 }   //End of class
